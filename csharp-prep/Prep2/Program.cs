d9b43b7384501498b3af836304a32582f68c6af9
Ryan Lyman <lyman43@byui.edu>
2024-01-13 13:14:19 -0700

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,54 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
 
-        //data types
-        int i;
-        long l;
-        float f;
-        double d;
-        char c;
-        string s;
-        byte b;
-        bool b2;
+        int gradePercent;
+        Console.WriteLine("What is your grade percent?");
+        string gradeInput = Console.ReadLine();
+        gradePercent = int.Parse(gradeInput);
+        string letter = "";
 
 
-        //variables
-        int myCount = 4;
-        string aName = "bob";
-
-
-        //var 
-        var anotherCount = 4;
-
-        //anotherCount = 3.4;
-
-        //printing Variables 
-        Console.Write("What is your age?");
-
-        //Readline
-
-        var ageString = Console.ReadLine();
-
-        System.Console.WriteLine($"My age is {ageString}.");
-        System.Console.WriteLine("My age is" +{
-            ageString + ".");
-
-            //convering Var 
-            int age = int.Parse(ageString);
-
-
-            //Conditionals
-            if (age < 18)
-            {
-                System.Console.WriteLine("You're a minor.");
-                System.Console.WriteLine("Another string to print");
-            }
-            else
-            {
-                System.Console.WriteLine("You're Old");
-            }
+        if (gradePercent >= 90)
+        {
+            letter = "A";
 
         }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercent < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+
+        if (gradePercent > 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed. Better luck next time.");
+        }
+
+
     }
+}
