6d8bf4ca871fc803a9026481d208c023b1836ea8
Ryan Lyman <ryanlyman@gmail.com>
2024-02-21 12:31:36 -0700

    Inhearatance

---
+++ csharp-prep/Inheratance/Program-example.cs
@@ -0,0 +1,87 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Learning04 World!");
+
+        Student s1 = new Student("0987654321", "Sam", "CIT");
+        Faculty f1 = new Faculty("1234567890", "John", "CSE");
+
+        List<Person> peopleList = new List<Person>();
+        peopleList.Add(s1);
+        peopleList.Add(f1);
+
+
+        foreach (var person in peopleList)
+        {
+            // person.major; 
+
+            // Console.WriteLine($"{person.DisplayInfo()}");
+            if (person is Student)
+            {
+                Student student = (Student)person;
+                student.Display();
+            }
+            else
+            {
+                Faculty faculty = (Faculty)person;
+                faculty.Display();
+            }
+        }
+    }
+}
+
+
+class Person
+{
+    private string id;
+    private string name;
+
+    public Person(string id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public string DisplayInfo()
+    {
+        return $"{name}: {id}";
+    }
+}
+
+class Student : Person
+{
+    private string major;
+
+    public Student(string id, string name, string major) : base(id, name)
+    {
+        this.major = major;
+    }
+
+    public void Display()
+    {
+
+        Console.WriteLine($"{DisplayInfo()} - {major}");
+    }
+}
+
+
+class Faculty : Person
+{
+    private string department;
+
+    public Faculty(string id, string name, string department) : base(id, name)
+    {
+        this.department = department;
+    }
+
+
+    public void Display()
+    {
+        Console.WriteLine($"{DisplayInfo()} - {department}");
+    }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment("Alex Tomson", "Sci");
+        MathAssignment mathAssignment1 = new MathAssignment("John Smith", "Math", "Section 7.3", "1-7");
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Sue", "Post WW2", "The Cold War");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+
+
+
     }
 }
